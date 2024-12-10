6964e1b87857bcf460b01f87222d70eddb3ec868
JhonpaulEspiritu13 <jhonpaul.espiritu13@gmail.com>
2024-12-10 03:38:04 -0600

    Added prefixing functionality

--- text_organizer_gui.py
+++ text_organizer_gui.py
@@ -60,18 +60,18 @@ class TextOrganizerGUI(tk.Tk):
         # Directory listbox
         self.directory_listbox_label = tk.Label(self.directory_frame, text="Directory List")
         self.directory_listbox_label.grid(row=0, column=0)
-        self.directory_listbox = tk.Listbox(self.directory_frame, width=15, height=9)
+        self.directory_listbox = tk.Listbox(self.directory_frame, width=15, height=9, selectmode="extended", exportselection=False)
         self.directory_listbox.grid(row=1, column=0)
-        #self.directory_listbox.bind("<<ListboxSelect>>", lambda event: self._change_selected_listbox(event, 0))
+        self.directory_listbox.bind("<<ListboxSelect>>", lambda event: self._change_selected_listbox_index(event, 0, "Directory"))
         self.listbox_list.append(self.directory_listbox)
         # Separator
         tkk.Separator(self.directory_frame, orient="horizontal").grid(row=2, sticky="ew", pady=3)
         # File Listbox
         self.file_listbox_label = tk.Label(self.directory_frame, text="File List")
         self.file_listbox_label.grid(row=3, column=0)
-        self.file_listbox = tk.Listbox(self.directory_frame, width=15, height=9)
+        self.file_listbox = tk.Listbox(self.directory_frame, width=15, height=9, selectmode="extended", exportselection=False)
         self.file_listbox.grid(row=4, column=0)
-        #self.file_listbox.bind("<<ListboxSelect>>", lambda event: self._change_selected_listbox(event, 1))
+        self.file_listbox.bind("<<ListboxSelect>>", lambda event: self._change_selected_listbox_index(event, 1, "File"))
         self.listbox_list.append(self.file_listbox)
 
     def _initialize_button_frame_elements(self):
@@ -82,7 +82,7 @@ class TextOrganizerGUI(tk.Tk):
         self.button_prefix_frame.grid(row=0)
         self._initialize_prefix_frame_buttons()
         # Sort frame
-        self.button_sort_frame = tk.LabelFrame(self.button_frame, width=125, height=100, text="Name Commands")
+        self.button_sort_frame = tk.LabelFrame(self.button_frame, width=125, height=120, text="Name Commands")
         self.button_sort_frame.grid_propagate(False)
         self.button_sort_frame.grid(row=1)
         self._initialize_sort_frame_buttons()
@@ -105,17 +105,27 @@ class TextOrganizerGUI(tk.Tk):
 
     def _initialize_sort_frame_buttons(self):
         """Initializes the button sort frame buttons."""
-        self.button_sort_frame_up_button = tk.Button(self.button_sort_frame, text="Sort Up", width=15, command=self.sort_up)
-        self.button_sort_frame_down_button = tk.Button(self.button_sort_frame, text="Sort Down", width=15)
-        self.button_sort_frame_up_button.grid(row=0)
-        self.button_sort_frame_down_button.grid(row=1)
+        # Label for deciding the currently selected list box.
+        self.button_sort_frame_selected_listbox_label = tk.Label(self.button_sort_frame, justify=tk.LEFT, anchor="w", font="bold 10", text="Selected Listbox:")
+        self.button_sort_frame_selected_listbox_label.grid(row=0)
+        self.button_sort_frame_selected_listbox_textbox = tk.Text(self.button_sort_frame, width=10, height=1, state="disabled", bg="gray90")
+        self.button_sort_frame_selected_listbox_textbox.grid(row=1)
+        # Separator
+        tkk.Separator(self.directory_frame, orient="horizontal").grid(row=2, sticky="ew", pady=3)
+        # Prefixing buttons, for actually adding prefixes to a listbox.
+        self.button_sort_frame_prefix_all_button = tk.Button(self.button_sort_frame, text="Prefix All", width=15, command=self._prefix_all)
+        self.button_sort_frame_prefix_some_button = tk.Button(self.button_sort_frame, text="Prefix Selected", width=15, command=self._prefix_selected)
+        self.button_sort_frame_prefix_all_button.grid(row=2)
+        self.button_sort_frame_prefix_some_button.grid(row=3)
 
     def _initialize_menu_bar_elements(self):
         """Initializes the elements for opening files/folders, closing files, etc."""
         # File Tab
         self.menu_bar_file = tk.Menu(self.menu_bar, tearoff=0)
         self.menu_bar_file.add_command(label="Open Directory", command=self.open_directory_dialog)
-        self.menu_bar.add_cascade(label="File", menu=self.menu_bar_file)
+        self.menu_bar_file.add_separator()
+        self.menu_bar_file.add_command(label="Refresh Directory", command=self.refresh_directory_files)
+        self.menu_bar.add_cascade(label="Folder", menu=self.menu_bar_file)
 
     # ================ Properties
 
@@ -191,12 +201,19 @@ class TextOrganizerGUI(tk.Tk):
         self.dict_directories = {}
         self.dict_files = {}
 
-    def _change_selected_listbox(self, event, value: int) -> None:
+    def _change_selected_listbox_index(self, event, index: int, name: str) -> None:
         """Bind function for listbox: Changes the value of the current
            selected list box index.
            
-           :param value: int - index to change"""
-        self.selected_list_box_index = value
+           :param index: int - index to change
+           :param name: str - name to set listbox"""
+        self.selected_list_box_index = index
+        # Textbox Change
+        self.button_sort_frame_selected_listbox_textbox.configure(state="normal")
+        self.button_sort_frame_selected_listbox_textbox.delete(1.0, tk.END)
+        self.button_sort_frame_selected_listbox_textbox.insert(tk.END, name)
+        self.button_sort_frame_selected_listbox_textbox.configure(state="disable")
+        
 
     def _change_selected_prefix_mode(self, value) -> None:
         """Command function for changing the current selected prefix.
@@ -215,13 +232,47 @@ class TextOrganizerGUI(tk.Tk):
             # Otherwise choose the mode.
             self.selected_prefix_base = PrefixBase[self.selected_prefix_mode.name].value
 
-    def sort_up(self):
-        """Checks if an element is selected, and sorts that element up."""
+    def _prefix_all(self):
+        """Add the current prefix to all elements."""
         selected_listbox = self.listbox_list[self.selected_list_box_index]
-        print(self.selected_list_box_index)
-        print(selected_listbox.curselection())
+        selected_dictionary = self.get_selected_dict()
+        # Loops through the selected items tuple.
+        all_items = selected_listbox.get(0, tk.END)
+        for actual_item in all_items:
+            # Selected item, and selected item with prefix.
+            new_item = self.selected_prefix_base + actual_item
+            # Pops the current item with the newly created prefixed item.
+            selected_dictionary[new_item] = selected_dictionary[actual_item]
+            del selected_dictionary[actual_item]
+        # Refreshes textbox because keys have been changed!
+        self._sort_listboxes()
+        self._refresh_listboxes()
+
+    def _prefix_selected(self):
+        """Add the current prefix to a select few elements."""
+        selected_listbox = self.listbox_list[self.selected_list_box_index]
+        selected_dictionary = self.get_selected_dict()
+        # Loops through the selected items tuple.
+        selected_items = selected_listbox.curselection()
+        for item in selected_items:
+            # Selected item, and selected item with prefix.
+            actual_item = selected_listbox.get(item)
+            new_item = self.selected_prefix_base + actual_item
+            # Pops the current item with the newly created prefixed item.
+            selected_dictionary[new_item] = selected_dictionary[actual_item]
+            del selected_dictionary[actual_item]
+        # Refreshes textbox because keys have been changed!
+        self._sort_listboxes()
+        self._refresh_listboxes()
+
+    def _sort_listboxes(self):
+        """Sorts the dictionaries."""
+        self.dict_directories = dict(sorted(self.dict_directories.items()))
+        self.dict_files = dict(sorted(self.dict_files.items()))
 
     def _refresh_listboxes(self):
+        for listbox in self.listbox_list:
+            listbox.delete(0,tk.END)
         """For each key in the listbox dictionaries, add it to the end of each listbox."""
         for directory in self.dict_directories.keys():
             self.directory_listbox.insert("end", directory)
@@ -229,6 +280,17 @@ class TextOrganizerGUI(tk.Tk):
             self.file_listbox.insert("end", file)
 
     # ================ Normal Functions
+
+    def get_selected_dict(self):
+        """Messy way of handling dictionary returns, but it seems
+           that storing dictionary references in a list does not work.
+           This will have to do for now."""
+        if self.selected_list_box_index == 0:
+            return self.dict_directories
+        elif self.selected_list_box_index == 1:
+            return self.dict_files
+        else:
+            return None
 
     def open_directory_dialog(self):
         """Opens a directory dialog to check a specified user directory."""
@@ -246,14 +308,25 @@ class TextOrganizerGUI(tk.Tk):
         except NoDirectoryPath:
             pass
 
+    def refresh_directory_files(self):
+        """Refreshes a selected directory."""
+        try:
+            self.open_directory_files()
+        except AttributeError:
+            # There is no directory to refresh.
+            tk.messagebox.showerror("No Directory Selected", "No directory has been selected to be refreshed.")
+            
+
     def open_directory_files(self):
         """Opens the directory and initializes them into a directory path list."""
         try:
+            self.dict_directories = {}
+            self.dict_files = {}
             self.file_directory.read_file_list()
             # For each value in the file dictionary, append it to the file directory.
             for directory in self.file_directory.get_directory_values():
                 self.dict_directories[directory] = directory
-            for file in self.file_directory.get_file_values() :
+            for file in self.file_directory.get_file_values():
                 self.dict_files[file] = file
             self._refresh_listboxes()
         except InvalidDirectoryPath:
